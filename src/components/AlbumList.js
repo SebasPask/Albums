18d7876c47556e0d1cf21d027ce57e470eb415bc
SebasPask <sebaspask@gmail.com>
2017-10-08 17:11:36 +1100

    first albums loaded

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import {View,Text} from 'react-native';
+import {View} from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
   componentWillMount(){
-    axois.get('https://rallycoding.herokuapp.com/api/music_albums')
-    .then(respose => this.setState({albums: respose.data}));
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({albums: response.data}));
+  }
+  renderAlbums(){
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album}/>
+    );
   }
     render(){
       return(
         <View>
-            <Text>Album list !!! :D</Text>
+          {this.renderAlbums()}
           </View>
         );
       }
